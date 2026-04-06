6e07fa29bc1ee2386d33da83022e1ae58b52a376
princek <princesamuelpks@gmail>
2026-04-06 11:05:56 +0000

    refactor code for easier reading

--- backendserver.py
+++ backendserver.py
@@ -29,15 +29,19 @@ run("cd /afribox/elearncore && sudo apt install python3-venv -y", "INSTALL PYTHO
 run("cd /afribox/elearncore && python3 -m venv venv && source venv/bin/activate", "CREATE AND ACTIVATE VIRTUAL ENV")
 run("cd /afribox/elearncore && pip install --upgrade pip setuptools", "UPGRADE PIP")
 run("cd /afribox/elearncore && pip install -r requirements.txt", "INSTALL REQUIREMENTS")
+
 # migrate database
 print("\n🗄️  Migrating database...")
 run("cd /afribox/elearncore && python manage.py makemigrations", "MIGRATE DATABASE")
 run("cd /afribox/elearncore && python manage.py migrate", "MIGRATE DATABASE")
+
+# setup Daphne and Nginx
 print("🚀 Setting up Afribox Backend Server...")
 run("sudo apt update", "STEP 1: UPDATE")
 run("sudo apt install -y python3-pip nginx", "STEP 2: INSTALL PACKAGES")
 run("pip install daphne", "STEP 3: INSTALL DAPHNE")
 
+# configure Daphne and Nginx
 print("\n🔧 Configuring Daphne and Nginx...")
 run("sudo cp deployment/websocket/afriboxdaphne.service /etc/systemd/system/afriboxdaphne.service", "STEP 4: CONFIGURE DAPHNE")
 run("sudo systemctl daemon-reload", "STEP 4: CONFIGURE DAPHNE")
